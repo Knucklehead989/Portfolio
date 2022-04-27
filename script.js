4dde8a4021b33dc571009ba63d0d3298ffedf75a
Knucklehead989 <98899970+Knucklehead989@users.noreply.github.com>
2022-04-27 13:39:19 +0545

    updatd slider

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
     var desc = document.getElementById("desc");
-    var hehe = document.getElementsByClassName("slider-text");
+    var hehe = document.getElementsByClassName("slider-desc");
 
     if (n> slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
@@ -110,7 +110,7 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
-    desc.innerHTML = hehe.innerText;
+    desc.innerHTML = hehe[slideIndex-1].innerText;
 }
 
 // smooth scrolling 
